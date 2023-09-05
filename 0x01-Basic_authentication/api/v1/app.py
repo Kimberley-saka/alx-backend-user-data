74dbc84a2c29e31644ad4c2262452bfb955584c1
Kimberley saka <kimberleysaka@gmail.com>
2023-09-05 16:54:28 +0200

    updated some things

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,9 +19,12 @@ if AUTH_TYPE is 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-elif AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+if "AUTH_TYPE" in os.environ:
+    if os.environ["AUTH_TYPE"] == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        auth = Auth()
 
 
 @app.before_request
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Basic uathentication
+Basic authentication
 """
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
