04d233eeb917dd86f43a9dd1ece8489b3928c872
Kimberley saka <kimberleysaka@gmail.com>
2023-09-05 16:17:33 +0200

    solution to task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,9 +12,22 @@ class Auth():
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        doc
+        require path
         """
-        return False
+        if path is None or (excluded_paths is None or len(excluded_paths) ==
+                            0):
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path.startswith(excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
