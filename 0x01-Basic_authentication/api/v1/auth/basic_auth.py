6e4cfda7748ea6b228b815c497e5d684255574c4
Kimberley saka <kimberleysaka@gmail.com>
2023-09-05 17:56:54 +0200

    updated for solution to task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic authentication
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -58,3 +59,25 @@ def extract_user_credentials(
 
     email, password = decoded_base64_authorization_header.split(':', 1)
     return (email, password)
+
+
+def user_object_from_credentials(self, user_email: str,
+                                 user_pwd: str) -> TypeVar('User'):
+    """
+    doc
+    """
+    if user_email is None or not isinstance(user_email, str):
+        return None
+
+    if user_pwd is None or not isinstance(user_pwd, str):
+        return None
+    user_list = User.search({"email": user_email})
+
+    if not user_list:
+        return None
+
+    for user in user_list:
+        if user.is_valid_password(user_pwd):
+            return user
+
+    return None
