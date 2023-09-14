a4386faf6ce5779b04d4d059b4d200f06a5b4859
Kimberley-saka <kimberleysaka@gmail.com>
2023-09-14 08:37:46 +0000

    fixed

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,14 +40,15 @@ def login():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if not AUTH.valid_login(email, password):
+
+    if not (AUTH.valid_login(email, password)):
         abort(401)
+
     session_id = AUTH.create_session(email)
-    if not session_id:
-        abort(401)
     response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return respons
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
