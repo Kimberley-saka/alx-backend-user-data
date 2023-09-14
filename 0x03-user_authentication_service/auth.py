358baeed858b48e0d96accdc67afd5c70f3f89db
Kimberley-saka <kimberleysaka@gmail.com>
2023-09-14 08:25:13 +0000

    fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,8 +67,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
         except NoResultFound:
             return None
-
-        user.session_id = _generate_uuid()
-        return user.session_id
