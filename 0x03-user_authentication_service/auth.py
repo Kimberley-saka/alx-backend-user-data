353af82a2d76574b5db944761c9477675ace2bb5
Kimberley-saka <kimberleysaka@gmail.com>
2023-09-14 08:13:42 +0000

    added session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,3 +60,14 @@ class Auth:
             return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        sessions
+        """
+        user = self._db.find_user_by(email=email)
+        if user is None:
+            return None
+
+        user.session_id = _generate_uuid()
+        return user.session_id
